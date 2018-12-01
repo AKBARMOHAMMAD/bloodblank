ef7e9ed92bf0e839f430ef51201911f1fd248665
AKBARMOHAMMAD <43230334+AKBARMOHAMMAD@users.noreply.github.com>
2018-12-01 11:16:01 +0530

    Add files via upload

--- bd/admin.py
+++ bd/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
+
 from .models import State
 from .models import City
 from .models import DonorRegister
-from .models import OriganizationRegister
+from .models import OrigranizationRegister
 
-# Register your models here.
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(DonorRegister)
-admin.site.register(OriganizationRegister)
+admin.site.register(OrigranizationRegister)
--- bd/models.py
+++ bd/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 
-# Create your models here.
 class State(models.Model):
-    idno=models.IntegerField(primary_key=True)
-    name=models.CharField(max_length=50)
+    idno = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+
 class City(models.Model):
-    idno=models.IntegerField(primary_key=True)
-    state_name=models.ForeignKey(State,on_delete=models.CASCADE)
-    city_name=models.CharField(max_length=50)
+    idno = models.IntegerField(primary_key=True)
+    state_name = models.ForeignKey(State,on_delete=models.CASCADE)
+    city_name = models.CharField(max_length=50)
+
 class DonorRegister(models.Model):
-    name=models.CharField(max_length=50)
-    contact_no=models.IntegerField()
-    city_name=models.ForeignKey(City,on_delete=models.CASCADE)
-    email_id=models.EmailField(max_length=50,primary_key=True)
-    password=models.CharField(max_length=50)
-class OriganizationRegister(models.Model):
-    name=models.CharField(max_length=50)
-    contact_no=models.IntegerField()
-    city_name=models.ForeignKey(City,on_delete=models.CASCADE)
-    email_id=models.EmailField(max_length=50,primary_key=True)
-    password=models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    contact_no = models.IntegerField()
+    city_name = models.ForeignKey(City,on_delete=models.CASCADE)
+    email_id = models.EmailField(max_length=100,primary_key=True)
+    password = models.CharField(max_length=50)
+
+class OrigranizationRegister(models.Model):
+    name = models.CharField(max_length=50)
+    contact	= models.IntegerField()
+    city = models.ForeignKey(City,on_delete=models.CASCADE)
+    email_id = models.EmailField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=50)
